7859577a24d10d946d0c174cd6cd3cd0253741b4
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2023-02-26 00:50:34 -0500

    proyecto base para backend

---
+++ src/main/java/me/popehiflo/invflobackend/InvfloBackendApplication.java
@@ -0,0 +1,13 @@
+package me.popehiflo.invflobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvfloBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvfloBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/popehiflo/invflobackend/InvfloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.popehiflo.invflobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvfloBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
